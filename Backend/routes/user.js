25bb08fe2ebf543a67a3dff7eae8e7137457c3ab
taulmav <haloodstboss@live.fr>
2021-09-26 02:49:20 +0200

    add email validation

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -5,11 +5,12 @@ const express = require('express')
 const router = express.Router()
 const userCtrl = require('../controllers/user')
 const ValPassword = require('../middleware/validatorPassword'); //Notre middleware de validation
+const ValEmail = require('../middleware/validatorEmail')
 
 /*********************************************************************************/
 //On créer nos routes post.
 
-router.post('/signup', ValPassword, userCtrl.signup) //Le middleware de validation vérifie le mot de passe et passe au signup.
+router.post('/signup', ValEmail, ValPassword, userCtrl.signup) //Le middleware de validation vérifie le mot de passe et passe au signup.
 router.post('/login',  userCtrl.login)
 
 /*********************************************************************************/
